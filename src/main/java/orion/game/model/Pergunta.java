986c4951cdfc521aa200a9f0268c156491b639c6
guismoreira <guilhermemoreira55@gmail.com>
2021-03-16 16:41:48 -0300

    entity-relationship

--- src/main/java/orion/game/model/Pergunta.java
+++ src/main/java/orion/game/model/Pergunta.java
@@ -1,5 +1,30 @@
 package orion.game.model;
-
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name = "pergunta")
 public class Pergunta {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    //...
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinTable(name = "game", 
+      joinColumns = 
+        { @JoinColumn(name = "pergunta_id", referencedColumnName = "id") },
+      inverseJoinColumns = 
+        { @JoinColumn(name = "resposta_id", referencedColumnName = "id") })
+    private Resposta resposta;
+
+    //... getters and setters
 }
