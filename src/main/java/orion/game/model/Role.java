9750ca395274cf86fbbcb06d80db741217a79443
guismoreira <guilhermemoreira55@gmail.com>
2021-03-29 15:09:53 -0300

    original files

---
+++ src/main/java/orion/game/model/Role.java
@@ -0,0 +1,35 @@
+package orion.game.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "ROLES")
+public class Role{
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+}
