9cd2c2bdc43738a4ed90e31448bcac4736a99ad1
eusouakau <56175615+eusouakau@users.noreply.github.com>
2022-02-23 02:27:18 -0300

    feat: (login) implements auth actions #32

---
+++ game-web/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Button variant="contained"> </Button>;
+        </div>
+    )
+}
