eb56dbf107e6853e2111bb81a3206c30852510b7
guismoreira <guilhermemoreira55@gmail.com>
2022-04-05 22:58:10 -0300

    cors and endpoint config

--- game-web/src/services/api.js
+++ game-web/src/services/api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
-export const api = axios.create({ baseURL: "http://localhost:3000" });
+export const api = axios.create({ baseURL: "http://localhost:8080/game/api/v1/" });
 
 export const createSession = async (email, password) => {
-    return api.post("/sessions", { email, password });
+    return api.post("/playerlogin", 
+    { email, password },
+    { headers: { 
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+     } }
+    );
 };
